7f3aa669c7eb8e1cd9a615e09a837125b4d32834
FlaviusPintican <flavius_ciprian99@yahoo.com>
2015-07-07 11:57:34 +0300

    initial comnit

---
+++ exemplututorial2.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>About W3Schools</h1>
+<p title="About W3Schools">
+	W3Schools is a web developer's site.
+	It provides tutorials and references covering
+	many aspects of web programming,
+	including HTML, CSS, JavaScript, XML, SQL, PHP, ASP, etc.
+</p>
+<p><b>
+<font color="red">	If you move the mouse over the paragraph above,
+					the title will display as a tooltip.
+</font>
+</b></p>
+<br>
+<style>
+a {text-decoration: none;}
+</style>
+<a href="http://www.w3schools.com">This is a link</a> <br><br>
+<img src="http://localhost:8080/internship/ex.jpg" width="104" height="142">
+
+</body>
+</html>
